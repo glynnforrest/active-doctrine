a29a67e340a08efe9b4f31e3691c857bc087286d
Glynn Forrest <me@glynnforrest.com>
2014-06-25 00:33:11 -0500

    Adding AbstractSelectorTest.

---
+++ tests/ActiveDoctrine/Tests/Selector/AbstractSelectorTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace ActiveDoctrine\Tests\Selector;
+
+use ActiveDoctrine\Selector\AbstractSelector;
+use Doctrine\DBAL\Connection;
+
+/**
+ * AbstractSelectorTest
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class AbstractSelectorTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function fromConnectionProvider()
+    {
+        return [
+            ['pdo_mysql', 'MysqlSelector']
+        ];
+    }
+
+    /**
+     * @dataProvider fromConnectionProvider()
+     */
+    public function testFromConnection($name, $class)
+    {
+        $driver = $this->getMock('Doctrine\DBAL\Driver');
+        $driver->expects($this->once())
+               ->method('getName')
+               ->will($this->returnValue($name));
+        $conn = $this->getMockBuilder('Doctrine\DBAL\Connection')
+                     ->disableOriginalConstructor()
+                     ->getMock();
+        $conn->expects($this->once())
+             ->method('getDriver')
+             ->will($this->returnValue($driver));
+
+        $selector = AbstractSelector::fromConnection($conn, 'table');
+        $this->assertInstanceOf(sprintf('ActiveDoctrine\Selector\%s', $class), $selector);
+    }
+
+    public function testFromConnectionThrowsException()
+    {
+        $driver = $this->getMock('Doctrine\DBAL\Driver');
+        $driver->expects($this->once())
+               ->method('getName')
+               ->will($this->returnValue('foo'));
+        $conn = $this->getMockBuilder('Doctrine\DBAL\Connection')
+                     ->disableOriginalConstructor()
+                     ->getMock();
+        $conn->expects($this->once())
+             ->method('getDriver')
+             ->will($this->returnValue($driver));
+
+        $this->setExpectedException('Doctrine\DBAL\DBALException');
+        AbstractSelector::fromConnection($conn, 'table');
+    }
+
+}
