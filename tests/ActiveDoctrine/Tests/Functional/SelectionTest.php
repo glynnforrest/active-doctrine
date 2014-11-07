a73052da098d57eb9d0b0a6ac46769bffff0e397
Glynn Forrest <me@glynnforrest.com>
2014-11-07 05:01:35 +0000

    More selection testing.

--- tests/ActiveDoctrine/Tests/Functional/SelectionTest.php
+++ tests/ActiveDoctrine/Tests/Functional/SelectionTest.php
@@ -2,7 +2,7 @@
 
 namespace ActiveDoctrine\Tests\Functional;
 
-use ActiveDoctrine\Tests\Entity\Book;
+use ActiveDoctrine\Tests\Fixtures\Entities\Bookshop\Book;
 
 /**
  * SelectionTest
@@ -28,6 +28,25 @@ class SelectionTest extends FunctionalTestCase
     public function testSelectOneEmpty()
     {
         $this->assertNull(Book::selectOne($this->getConn())->execute());
+    }
+
+    public function testSelectCountEmpty()
+    {
+        $this->assertSame(0, Book::select($this->getConn())->count()->execute());
+    }
+
+    public function testSelect()
+    {
+        $this->loadData('bookshop');
+        $books = Book::select($this->getConn())
+            ->execute();
+        $this->assertInstanceOf('ActiveDoctrine\Entity\EntityCollection', $books);
+        $this->assertSame(50, count($books));
+        $book = $books[0];
+        $this->assertInstanceOf(Book::CLASS, $book);
+        $this->assertSame('Book 1', $book->name);
+        $this->assertSame('The very first book', $book->description);
+        $this->assertSame('1', $book->authors_id);
     }
 
 }
