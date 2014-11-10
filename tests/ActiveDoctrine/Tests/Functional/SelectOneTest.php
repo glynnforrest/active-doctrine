1d197a1c2f6f7d1bac33bd738804e3ac2ec4f0be
Glynn Forrest <me@glynnforrest.com>
2014-11-10 15:02:14 +0000

    Asserting that selecting one entity works with both methods.

--- tests/ActiveDoctrine/Tests/Functional/SelectOneTest.php
+++ tests/ActiveDoctrine/Tests/Functional/SelectOneTest.php
@@ -17,26 +17,44 @@ class SelectOneTest extends FunctionalTestCase
         $this->loadSchema('bookshop');
     }
 
-    public function testEmptyTable()
+    /**
+     * Check that selecting one entity works with both methods.
+     */
+    public function selectorProvider()
     {
-        $this->assertNull(Book::selectOne($this->getConn())->execute());
+        return [
+            [Book::selectOne($this->getConn())],
+            [Book::select($this->getConn())->one()]
+        ];
     }
 
-    public function testSingleId()
+    /**
+     * @dataProvider selectorProvider()
+     */
+    public function testEmptyTable($selector)
+    {
+        $this->assertNull($selector->execute());
+    }
+
+    /**
+     * @dataProvider selectorProvider()
+     */
+    public function testSingleId($selector)
     {
         $this->loadData('bookshop');
-        $book = Book::selectOne($this->getConn())
-            ->where('id', '=', 3)
+        $book = $selector->where('id', '=', 3)
             ->execute();
         $this->assertInstanceOf('ActiveDoctrine\Tests\Fixtures\Entities\Bookshop\Book', $book);
         $this->assertSame('3', $book->id);
     }
 
-    public function testOneFromManyResults()
+    /**
+     * @dataProvider selectorProvider()
+     */
+    public function testOneFromManyResults($selector)
     {
         $this->loadData('bookshop');
-        $book = Book::selectOne($this->getConn())
-            ->orderBy('id', 'DESC')
+        $book = $selector->orderBy('id', 'DESC')
             ->limit(100)
             ->execute();
         $this->assertInstanceOf('ActiveDoctrine\Tests\Fixtures\Entities\Bookshop\Book', $book);
