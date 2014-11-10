233ca5e49153fa8021e90a2d6dc6c2a45e999aaa
Glynn Forrest <me@glynnforrest.com>
2014-11-10 14:56:33 +0000

    Further testing selectOne() behaviour.

--- tests/ActiveDoctrine/Tests/Functional/SelectOneTest.php
+++ tests/ActiveDoctrine/Tests/Functional/SelectOneTest.php
@@ -22,4 +22,25 @@ class SelectOneTest extends FunctionalTestCase
         $this->assertNull(Book::selectOne($this->getConn())->execute());
     }
 
+    public function testSingleId()
+    {
+        $this->loadData('bookshop');
+        $book = Book::selectOne($this->getConn())
+            ->where('id', '=', 3)
+            ->execute();
+        $this->assertInstanceOf('ActiveDoctrine\Tests\Fixtures\Entities\Bookshop\Book', $book);
+        $this->assertSame('3', $book->id);
+    }
+
+    public function testOneFromManyResults()
+    {
+        $this->loadData('bookshop');
+        $book = Book::selectOne($this->getConn())
+            ->orderBy('id', 'DESC')
+            ->limit(100)
+            ->execute();
+        $this->assertInstanceOf('ActiveDoctrine\Tests\Fixtures\Entities\Bookshop\Book', $book);
+        $this->assertSame('50', $book->id);
+    }
+
 }
