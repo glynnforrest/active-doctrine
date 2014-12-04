616e75ad0a24da3b4a20bc2ff001daf0e27b2a92
Glynn Forrest <me@glynnforrest.com>
2014-12-04 11:42:20 +0000

    Adding AbstractRepository#findAll().

--- src/ActiveDoctrine/Repository/AbstractRepository.php
+++ src/ActiveDoctrine/Repository/AbstractRepository.php
@@ -12,17 +12,39 @@ use Doctrine\DBAL\Connection;
 abstract class AbstractRepository
 {
     protected $conn;
-    protected $entity_class = 'unknown';
+    protected $entity_class;
 
     public function __construct(Connection $connection)
     {
         $this->conn = $connection;
     }
 
+    /**
+     * Create a new Entity.
+     *
+     * @return Entity
+     */
     public function create()
     {
         $class = $this->entity_class;
 
         return new $class($this->conn);
+    }
+
+    protected function selector()
+    {
+        $class = $this->entity_class;
+
+        return $class::select($this->conn);
+    }
+
+    /**
+     * Find all Entities.
+     *
+     * @return EntityCollection A collection of entities
+     */
+    public function findAll()
+    {
+        return $this->selector()->execute();
     }
 }
--- tests/ActiveDoctrine/Tests/Repository/AbstractRepositoryTest.php
+++ tests/ActiveDoctrine/Tests/Repository/AbstractRepositoryTest.php
@@ -16,14 +16,47 @@ class AbstractRepositoryTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
+        $driver = $this->getMock('Doctrine\DBAL\Driver');
+        $driver->expects($this->any())
+               ->method('getName')
+               ->will($this->returnValue('pdo_mysql'));
         $this->conn = $this->getMockBuilder('Doctrine\DBAL\Connection')
                            ->disableOriginalConstructor()
                            ->getMock();
+        $this->conn->expects($this->any())
+                   ->method('getDriver')
+                   ->will($this->returnValue($driver));
         $this->repo = new BookRepository($this->conn);
     }
 
     public function testCreate()
     {
         $this->assertInstanceOf('ActiveDoctrine\Tests\Fixtures\Entities\Bookshop\Book', $this->repo->create());
+    }
+
+    public function testFindAll()
+    {
+        $stmt = $this->getMockBuilder('Doctrine\DBAL\Statement')
+                     ->disableOriginalConstructor()
+                     ->getMock();
+        $stmt->expects($this->once())
+             ->method('execute')
+             ->with([]);
+        $stmt->expects($this->exactly(3))
+             ->method('fetch')
+             ->will($this->onConsecutiveCalls(
+                 ['name' => 'foo', 'id' => 4],
+                 ['name' => 'bar', 'id' => 5],
+                 false
+             ));
+        $this->conn->expects($this->once())
+                   ->method('prepare')
+                   ->with('SELECT * FROM `books`')
+                   ->will($this->returnValue($stmt));
+
+        $books = $this->repo->findAll();
+        $this->assertInstanceOf('ActiveDoctrine\Entity\EntityCollection', $books);
+        $this->assertSame(2, count($books));
+        $this->assertSame(['foo', 'bar'], $books->getColumn('name'));
     }
 }
