5984ca3258c2a314dd829496b87453b22d3d4d44
Glynn Forrest <me@glynnforrest.com>
2014-12-13 18:01:09 +0000

    Adding functional repository test.

---
+++ tests/ActiveDoctrine/Tests/Functional/RepositoryTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace ActiveDoctrine\Tests\Functional;
+
+use ActiveDoctrine\Tests\Fixtures\Bookshop\BookRepository;
+
+/**
+ * RepositoryTest
+ *
+ * @author Glynn Forrest <me@glynnforrest.com>
+ **/
+class RepositoryTest extends FunctionalTestCase
+{
+    protected $repo;
+
+    public function setup()
+    {
+        $this->repo = new BookRepository($this->getConn());
+        $this->loadSchema('bookshop');
+    }
+
+    public function testEmptyTable()
+    {
+        $books = $this->repo->findAll();
+        $this->assertInstanceOf('ActiveDoctrine\Entity\EntityCollection', $books);
+        $this->assertSame(0, count($books));
+    }
+
+    public function testFindAll()
+    {
+        $this->loadData('bookshop');
+        $books = $this->repo->findAll();
+
+        $this->assertInstanceOf('ActiveDoctrine\Entity\EntityCollection', $books);
+        $this->assertSame(50, count($books));
+        $book = $books[0];
+        $this->assertInstanceOf('ActiveDoctrine\Tests\Fixtures\Bookshop\Book', $book);
+        $this->assertSame('Book 1', $book->name);
+        $this->assertSame('The very first book', $book->description);
+        $this->assertSame('1', $book->authors_id);
+    }
+
+    public function testFindBy()
+    {
+        $this->loadData('bookshop');
+        $books = $this->repo->findBy(['id' => 3]);
+
+        $this->assertInstanceOf('ActiveDoctrine\Entity\EntityCollection', $books);
+        $this->assertSame(1, count($books));
+        $book = $books[0];
+        $this->assertInstanceOf('ActiveDoctrine\Tests\Fixtures\Bookshop\Book', $book);
+        $this->assertSame('Book 3', $book->name);
+        $this->assertSame('Book 3 description', $book->description);
+        $this->assertSame('3', $book->authors_id);
+    }
+
+    public function testFindOneBy()
+    {
+        $this->loadData('bookshop');
+        $book = $this->repo->findOneBy(['id' => 3]);
+
+        $this->assertInstanceOf('ActiveDoctrine\Tests\Fixtures\Bookshop\Book', $book);
+        $this->assertSame('Book 3', $book->name);
+        $this->assertSame('Book 3 description', $book->description);
+        $this->assertSame('3', $book->authors_id);
+    }
+}
