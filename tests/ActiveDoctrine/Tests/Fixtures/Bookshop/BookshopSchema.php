c96afc9c21339f0392b2e43e445a22d1d8567639
Glynn Forrest <me@glynnforrest.com>
2015-01-20 23:07:53 +0000

    Adding book_details to BookshopSchema.

--- tests/ActiveDoctrine/Tests/Fixtures/Bookshop/BookshopSchema.php
+++ tests/ActiveDoctrine/Tests/Fixtures/Bookshop/BookshopSchema.php
@@ -22,11 +22,21 @@ class BookshopSchema implements SchemaInterface
         $table->addColumn('name', 'string', ['length' => '255']);
         $table->addColumn('description', 'text');
         $id = $table->addColumn('authors_id', 'integer', ['unsigned' => true]);
+
+        $table = $schema->createTable('book_details');
+        $id = $table->addColumn('id', 'integer', ['unsigned' => true]);
+        $id->setAutoIncrement(true);
+        $table->setPrimaryKey(['id']);
+        $table->addColumn('books_id', 'integer', ['unsigned' => true]);
+        $table->addColumn('synopsis', 'text');
+        $table->addColumn('pages', 'integer', ['unsigned' => true]);
+        $table->addColumn('chapters', 'integer', ['unsigned' => true]);
     }
 
     public function down(Schema $schema)
     {
         $schema->dropTable('books');
+        $schema->dropTable('book_details');
     }
 
 }
