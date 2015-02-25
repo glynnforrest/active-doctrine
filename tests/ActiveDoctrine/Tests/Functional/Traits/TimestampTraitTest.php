9118263832b7fd69279e02ea6774cd4a95d805d0
Glynn Forrest <me@glynnforrest.com>
2015-02-25 19:07:52 +0000

    Adding more TimestampTrait testing.

--- tests/ActiveDoctrine/Tests/Functional/Traits/TimestampTraitTest.php
+++ tests/ActiveDoctrine/Tests/Functional/Traits/TimestampTraitTest.php
@@ -26,4 +26,31 @@ class TimestampTraitTest extends FunctionalTestCase
         $this->assertEquals(new \DateTime(), $article->created_at);
         $this->assertEquals(new \DateTime(), $article->updated_at);
     }
+
+    public function testAddTimestampsForDifferentEntities()
+    {
+        $article = new Article($this->getConn());
+        $article->title = 'Foo';
+        $article->save();
+        $writer = new Writer($this->getConn());
+        $writer->save();
+
+        $datetime = new \DateTime();
+        $this->assertEquals($datetime, $article->created_at);
+        $this->assertEquals($datetime, $article->updated_at);
+
+        $this->assertEquals($datetime, $writer->createdAt);
+        $this->assertEquals($datetime, $writer->anotherCreate);
+        $this->assertEquals($datetime, $writer->updatedAt);
+        $this->assertEquals($datetime, $writer->anotherUpdate);
+
+        //check that other fields haven't been set on different entities
+        $this->assertNull($writer->created_at);
+        $this->assertNull($writer->updated_at);
+
+        $this->assertNull($article->createdAt);
+        $this->assertNull($article->anotherCreate);
+        $this->assertNull($article->updatedAt);
+        $this->assertNull($article->anotherUpdate);
+    }
 }
