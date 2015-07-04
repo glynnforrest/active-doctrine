650b5d7661ff6845db25bc4ce7a04b2533c9c640
Glynn Forrest <me@glynnforrest.com>
2015-07-04 19:51:42 +0100

    Testing for eager loading across 3 entities.

--- tests/ActiveDoctrine/Tests/Functional/SelectEagerLoadTest.php
+++ tests/ActiveDoctrine/Tests/Functional/SelectEagerLoadTest.php
@@ -31,4 +31,27 @@ class SelectEagerLoadTest extends FunctionalTestCase
         //only two queries should have run
         $this->assertSame(2, $this->getQueryCount());
     }
+
+    public function testSelectWithEagerTwoSteps()
+    {
+        $this->loadData('bookshop');
+        $this->resetQueryCount();
+        $authors = Author::select($this->getConn())
+                 ->with('books', function($q) {
+                     $q->with('details');
+                 })->execute();
+
+        //simulation of some application logic
+        foreach ($authors as $author) {
+            if (count($author->books) < 1 || !$author->books[0]->has('details')) {
+                continue;
+            }
+
+            $author->books[0]->details->synposis;
+        }
+
+        $this->assertSame($authors[1]->books[0]->details->synopsis, 'Something something something');
+        //only three queries: authors, books and details
+        $this->assertSame(3, $this->getQueryCount());
+    }
 }
